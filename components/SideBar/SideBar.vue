cc70660aa30a25f9e279f2ab0d3918bb73c03fdb
Sören Oehding <oehding@werk4.net>
2018-07-26 16:13:04 +0200

    Added Pages and implemented basic route-transitions

--- components/SideBar/SideBar.vue
+++ components/SideBar/SideBar.vue
@@ -3,22 +3,22 @@
     <aside class="menu">
       <app-logo/>
       <ul class="menu-list">
-        <li><a>Home</a></li>
-        <li><a>About</a></li>
-        <li><a>Clients</a></li>
-        <li><a>Portfolio</a></li>
+        <li><nuxt-link to="/">Home</nuxt-link></li>
+        <li><nuxt-link to="/about">About</nuxt-link></li>
+        <li><nuxt-link to="/portfolio">Portfolio</nuxt-link></li>
+        <li><nuxt-link to="/projects">Projects</nuxt-link></li>
       </ul>
       <div class="block connect has-text-centered">
-        <span class="icon is-large has-text-white">
+        <span class="icon is-medium is-rounded has-background-primary has-text-dark">
           <i class="socicon-github"></i>
         </span>
-        <span class="icon is-large has-text-white">
+        <span class="icon is-medium is-rounded has-background-primary has-text-dark">
           <i class="socicon-linkedin"></i>
         </span>
-        <span class="icon is-large has-text-white">
+        <span class="icon is-medium is-rounded has-background-primary has-text-dark">
           <i class="socicon-xing"></i>
         </span>
-        <span class="icon is-large has-text-white">
+        <span class="icon is-medium is-rounded has-background-primary has-text-dark">
           <i class="socicon-soundcloud"></i>
         </span>
       </div>
@@ -50,7 +50,6 @@
 
 <style lang="scss">
   .is-sidebar {
-
     background: $grey-dark;
     position: fixed;
     top: 0;
@@ -62,10 +61,21 @@
     padding: 2.5rem;
     background: $grey-dark;
 
-    li {
-      a {
-        color: $white;
+    .menu {
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+      justify-content: space-between;
+      li {
+        a {
+          color: $white;
+        }
       }
+    }
+  }
+  .connect {
+    .icon {
+      margin-right: .5rem;
     }
   }
 </style>
---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        ABOUT PAGE
+      </h1>
+      <h2 class="subtitle">
+        Frontend Application for my personal Website
+      </h2>
+    </div>
+  </section>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  components: {
+    AppLogo
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
