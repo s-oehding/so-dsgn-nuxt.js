b6a7c08cfe7524edf803ff57b259f707088f133b
mono <soeren.oehding@gmail.com>
2018-08-14 09:45:22 +0200

    added content class

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,24 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        pxlwrx.de
-      </h1>
-      <h2 class="subtitle">
-        Frontend Application for my personal Website
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+  <section class="container content">
+    <div class="columns">
+      <div class="column">
+        <app-logo/>
+        <h1 class="title">
+          pxlwrx.de
+        </h1>
+        <h2 class="subtitle">
+          Frontend Application for my personal Website
+        </h2>
+        <div class="links">
+          <a
+            href="https://nuxtjs.org/"
+            target="_blank"
+            class="button--green">Documentation</a>
+          <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+            class="button--grey">GitHub</a>
+        </div>
       </div>
     </div>
   </section>
@@ -36,5 +38,15 @@ export default {
 </script>
 
 <style lang="scss">
+  .container {
+    background-size: cover;
+    height: 100vh;
+  }
+  .content {
+    padding: 0 1rem;
+  }
+  #main-content {
 
+
+  }
 </style>
